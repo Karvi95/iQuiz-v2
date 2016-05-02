677c2a9c9b02e78c3254a4f68bb87df7c7e44d54
karvi90 <karvi90@uw.edu>
2016-05-02 12:26:46 -0700

    First Push

---
+++ iQuiz-v2/iQuiz/QuizComponentCell.swift
@@ -0,0 +1,30 @@
+//
+//  QuizComponentCell.swift
+//  iQuiz
+//
+//  Created by iGuest on 4/29/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class QuizComponentCell: UITableViewCell {
+
+    @IBOutlet weak var topicPhoto: UIImageView!
+    @IBOutlet weak var topicName: UILabel!
+    @IBOutlet weak var topicDescr: UILabel!
+    
+    @IBOutlet weak var topicAuthor: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iQuiz-v2/iQuiz/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by iGuest on 4/29/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIAlertViewDelegate {
+
+    func dismissAlert(alert: UIAlertAction!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func showAlert(sender: AnyObject) {
+        let alertController : UIAlertController = UIAlertController(title: "Alert!", message: "Settings go here.", preferredStyle: .Alert)
+        let okAction : UIAlertAction = UIAlertAction(title: "Okay", style: .Default, handler: dismissAlert)
+
+        alertController.addAction(okAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+
+    @IBOutlet weak var QuizTable: UITableView!
+    var names = ["Mathematics", "Marvel Super Heroes", "Science"]
+    var descrs = ["Once Euler looked out \nat seven bridges to cross. \n'Can’t be done,' he said.", "What can go wrong now? \nGuns, gods, monsters, heroes too! \nNot-death, shwarma break.", "Contract or expand? \nNot the universe's size \nBut the human mind."]
+    var images = [UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3")]
+    var authors = ["- Tao Wang", "- Anonymous" ,"- Cassandra Teas"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.QuizTable.dequeueReusableCellWithIdentifier("QuizCell", forIndexPath: indexPath) as! QuizComponentCell
+        
+        cell.topicPhoto.image = images[indexPath.row]
+        cell.topicName.text = names[indexPath.row]
+        cell.topicDescr.text = descrs[indexPath.row]
+        cell.topicAuthor.text = authors[indexPath.row]
+        
+        return cell
+    }
+}
+
