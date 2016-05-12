1584019a1a1166748f73fafa7f8b03990bc0ebb7
karvi90 <karvi90@uw.edu>
2016-05-11 21:26:20 -0700

    Started filePath stuff

--- iQuiz-v2/iQuiz/QuizModel.swift
+++ iQuiz-v2/iQuiz/QuizModel.swift
@@ -23,18 +23,33 @@ class Data {
     var mathQs : [Question] = []
     var marvelQs : [Question] = []
 
+    func checkLocalStorage() {
+        //Check if json exists
+
+//        let path = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        let tempDirURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent(NSProcessInfo.processInfo().globallyUniqueString, isDirectory: true)
+        let filePath = tempDirURL.URLByAppendingPathComponent("quiz.json").absoluteString
+        let fileManager = NSFileManager.defaultManager()
+        if fileManager.fileExistsAtPath(filePath) {
+            print("FILE AVAILABLE")
+        } else {
+            print("FILE NOT AVAILABLE")
+        }
+
+    }
     
     
     func HTTPRequest(completionHandler: () -> Void) {
         let task = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: targetURL)!) { (data, response, error) -> Void in
-//            print(self.targetURL)
+//          print(self.targetURL)
             let HTTPResponse = response as! NSHTTPURLResponse
             let statusCode = HTTPResponse.statusCode
-        
+            
             if (statusCode == 200) {
                 do {
                     let json = try NSJSONSerialization.JSONObjectWithData(data!, options: [])
-//                     print(json)
+                    
+                    // print(json)
                 
                     guard let subject = json as? [[String : AnyObject]] else {return}
 
